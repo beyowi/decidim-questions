4a5d5ec0434480dde461bdf1e9f9f5af8dc8935e
cedrtang <cedric@beyowi.com>
2021-01-29 20:39:06 -0800

    Squash migrations

--- db/migrate/20191206154128_add_endorsements_counter_cache_to_questions.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddEndorsementsCounterCacheToQuestions < ActiveRecord::Migration[5.2]
-  def change
-    add_column :decidim_questions_questions, :endorsements_count, :integer, null: false, default: 0
-  end
-end
--- db/migrate/20200227175922_add_state_published_at_to_questions.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddStatePublishedAtToQuestions < ActiveRecord::Migration[5.2]
-  def change
-    add_column :decidim_questions_questions, :state_published_at, :datetime
-  end
-end
